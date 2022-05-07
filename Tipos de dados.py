8238f2125a9cc8e38e16e843c4c7773aa6b096c3
Alexandre Silva <alexandre.87mc@gmail.com>
2022-05-06 21:08:58 -0300

    Create Tipos de dados.py

---
+++ Tipos de dados.py
@@ -0,0 +1,27 @@
+#Tipos de numeros em Python
+#Inteiros e ponto flutuante
+print(type(1))
+print(type(1.1))
+
+#Atribuicao e operacoes com variaveis
+a = 10
+print(a)
+a = a - 2
+print(a)
+
+#Strings - são imutáveis, mas concatenaveis
+print(type('ola'))
+print("Olá")
+print("Olá, tudo bem com vocês")
+print('A seguir uma "citação".')
+print("A seguir uma 'citação'.")
+print("Olá \n Agora \t Enter e muito espaço")
+s = "jose amado"
+print(s)
+print(len(s))
+print(s[2])
+print(s[2:])
+print(s[::2])
+s = s + " meu filho"
+print(s)
+print(s.split())
