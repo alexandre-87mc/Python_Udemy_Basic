e9d9e773111c422bf39adaf6b107b538d0e5dd9a
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 12:35:22 -0300

    Update and rename Operadores de comparacao.py to  Comparison_operators.py

--- Comparison_operators.py
+++ Comparison_operators.py
@@ -1,12 +1,18 @@
-#Operadores de comparação
+#Comparison operators
+
+#equal
 print(2==2)
+#greater
 print(2>1)
+#equal or greater
 print(22>=21)
+#equal or less
 print(30<=5)
 print(5<=30)
+#not equal
 print(5!=5)
 
-#Operadores de comparação em cadeia
+#Chain comparison operators
 a = 1
 b = 2
 c = 3
@@ -15,4 +21,3 @@ print(a==1 and b==3)
 print(a==1 or b==1)
 print(a!=b or a==c)
 print(a==b or b==c)
-
