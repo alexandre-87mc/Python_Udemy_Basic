cc8331a255421891edd833636c0df9914cfae29c
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 10:04:12 -0300

    Update and rename Compreensao em litas.py to Lists_understanding.py

--- Lists_understanding.py
+++ Lists_understanding.py
@@ -1,18 +1,19 @@
-#Compreensao em listas
-#Exemplo de preenchimento de lista
+#Lists understanding
+
+#Exemple of list fill
 x = []
 for i in range(0,30):
     x += [i]
     print(x)
 
-#Exemplo de preenchimento de lista simplificado
+#Exemple of list simplifyed fill
 x2 = [i for i in range(0,30)]
 print(x2)
 
 x3 = [i*2+10 for i in range(0,15)]
 print(x3)
 
-#Listas pares
+#Pair lists
 x = []
 for i in range(0,20):
     if i%2==0:
@@ -22,11 +23,11 @@ for i in range(0,20):
 x4 = [i for i in range(0,10) if i % 2 == 0]
 print(x4)
 
-#Listas strings
+#Strings lists
 lista = [letter for letter in 'word']
 print(lista)
 
-#Conversão de temperatura ºC para Fahrenheit
+#Convertion ºC to Fahrenheit
 celsius = [0,10,15,20,30,50,100]
 fahrenheit = [(temp*(9/5)+32) for temp in celsius]
 print(fahrenheit)
