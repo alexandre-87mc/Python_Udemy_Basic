48493522985bce5060f48cb9b799265373a9ea3d
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 10:19:59 -0300

    Update Declaracoes em Python.py

--- Declaracoes em Python.py
+++ Declaracoes em Python.py
@@ -1,32 +1,32 @@
-#Declarações em Python
+#Python declarations
 a = 1
 b = 11
 if a>b:
-    print('Sim, %i é maior que %i' %(a,b))
+    print('Yes, %i é smaller then %i' %(a,b))
 else:
-    print('Não, %i não é maior que %i' %(a,b))
+    print('No, %i is not bigger then %i' %(a,b))
 
-#Testes com If e elif
+#Testes with If and elif
 lista = [1,2,3,4,5,6,7,8,9,10]
 if lista[3] >= 9:
-    print('Aprovado!')
+    print('Aprooved!')
 elif lista[5] <= 6:
-    print('Reprovado!')
+    print('Failed!')
 else:
-    print('Não sei mais o que fazer.')
+    print("I don't know.")
 
-#Declaração de um dicionario
-Dicionario = {'Adriano':10,'Paulo':7,'Joao':3}
-j='Adriano'
-k='Paulo'
-l='Joao'
+#Diccionary declaration
+Diccio = {'Adrian':10,'Paul':7,'John':3}
+j='Adrian'
+k='Paul'
+l='John'
 
-#Testes com if else
-if Dicionario[j] >= 9:
-    print('Parabéns! Você foi ótimo!')
-if Dicionario[k] >= 6:
-    print('Parabéns, você passou!')
-if Dicionario[l] >= 6:
-    print('Parabéns, você passou!')
+#Testes with if else
+if Diccio[j] >= 9:
+    print('Well done! Great!')
+if Diccio[k] >= 6:
+    print('Well done! You did it')
+if Diccio[l] >= 6:
+    print('Well done! You did it')
 else:
-    print("Que pena, você não passou!")
+    print("Too bad you did not pass!")
