afb6e9d5f87041958c4ab510d867d7507d1e02d7
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 14:15:07 -0300

    Update and rename Sets_e_Booleanos.py to Sets_and_Booleans.py

---
+++ Sets_and_Booleans.py
@@ -0,0 +1,18 @@
+#Sets e booleans
+
+#Create a set and add items
+x = set()
+print(x)
+x.add(1)
+print(x)
+x.add(2)
+print(x)
+
+#Utility
+list1 = [11,11,11,22,22,3,4,1,2,2]
+set_list1 = set(list1)
+print(set_list1)
+
+#Booleans
+a = True
+print(1>2)
