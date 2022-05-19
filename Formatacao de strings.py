71544fc48edd575dc01464533978c95eb1d5ebfe
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 10:40:30 -0300

    Update and rename Formatacao de strings.py to String_formatting.py

--- Formatacao de strings.py
+++
@@ -1,7 +0,0 @@
-#Formatação de impressão
-s = 'string'
-print('Temos uma %s auxiliar.' %(s))
-print('Print de numero flutuante: %.5f' %(13.44))
-s = 'string1'
-f = 11.456789
-print('Print de uma string %s. Print de um float %r' %(s,f))
---
+++ String_formatting.py
@@ -0,0 +1,13 @@
+#String formatting
+
+#string initializing
+s = 'string'
+
+#print string and float using mask
+print('We have a %s aux.' %(s))
+print('Print float number: %.5f' %(13.44))
+
+#print string and float using mask in the same sentence
+s = 'string1'
+f = 11.456789
+print('Print a string %s. Print a float %r' %(s,f))
