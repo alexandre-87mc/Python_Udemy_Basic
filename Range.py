30a0d1274530ce44dfe48ead4a988fb5f72aad78
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 14:09:41 -0300

    Update Range.py

--- Range.py
+++ Range.py
@@ -1,12 +1,12 @@
-#Método RANGE
+#Renge method
 rangee = range(0,10,1)
-#exemplo
+#exemple
 for i in range(0,100):
     print(i+2)
     if i>10:
         break
 
-
+#print list created by range
 print(list(range(0,10)))
-lista = list(range(0,10))
-print(lista)
+list1 = list(range(0,10))
+print(list1)
