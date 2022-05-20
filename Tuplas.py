5c8a24fd9a95de25006cebb3e5dc5de05f6b7dfa
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 22:18:08 -0300

    Update Tuplas.py

--- Tuplas.py
+++ Tuplas.py
@@ -1,8 +1,8 @@
-#Tuplas - imutabilidade
+#Tuples - immutability
 t = (1,2,3)
-#Métodos
+
+#Methods
 print(t.index(2))
 print(t.count(1))
 print(t[1])
-# não é possível --> t[1]=1
-t.
+# not possible --> t[1]=1
