3eeba90cddfb55d2c9ce45d3a66ebd92d02ab439
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-05-19 22:16:48 -0300

    Update and rename Tipos de dados.py to Data_type.py

---
+++ Data_type.py
@@ -0,0 +1,30 @@
+#Python data type
+
+#Int and float
+print(type(1))
+print(type(1.1))
+
+#Assignment and operations with variables
+a = 10
+print(a)
+a = a - 2
+print(a)
+
+#Strings - are immutable, but concatenatable
+print(type('Hi'))
+print("HI")
+print("HI, how are you")
+print('The following is a "quote".')
+print("The following is a 'quote'.")
+print("Hi \n Now \t Enter a lot of space")
+s = "Love John"
+print(s)
+print(len(s))
+print(s[2])
+print(s[2:])
+print(s[::2])
+
+#Concatenate
+s = s + " my son"
+print(s)
+print(s.split())
